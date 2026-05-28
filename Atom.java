e825242f88281d4bed8fd03f63536309c339d6d5
Kuranez <kuranez420@proton.me>
2026-05-28 19:10:40 +0200

    Extension to hydrogenlike atoms

--- Atom.java
+++ Atom.java
@@ -16,9 +16,11 @@ public class Atom {
     }
 
     //Methoden
-    public double calculateEnergyLevel(int n) {
-        // Bohrmodell: E_n = -13.6 eV / n^2, wobei n die Hauptquantenzahl ist
-        return -13.6 / (n * n); 
+    public double calculateEnergyLevel(int Z, int n) {
+        // Wasserstoffähnliche Kerne: Z = 1, 2, 8, ...
+        // Für Wasserstoff: Z = 1, für Helium: Z = 2, für Sauerstoff: Z = 8
+        // Bohrmodell: E_n = -13.6 eV * Z^2 / n^2, wobei n die Hauptquantenzahl ist
+        return -13.6 * Z * Z / (n * n); 
     }
 
     //Getter
--- Main.java
+++ Main.java
@@ -4,17 +4,54 @@ public class Main {
         // System.out.println("Test!");
 
         Atom hydrogen = new Atom(
-            "Wasserstoff", 
+            "Wasserstoff (H)", 
             1, 
-            1
+            1 
+        );
+
+        Atom helium = new Atom(
+            "Helium (He+)", 
+            2, 
+            2 
+            // maximale Elektronenzahl in der ersten Schale ist 2, aber Helium hat insgesamt 2 Elektronen
+        );
+
+        Atom oxygen = new Atom(
+            "Sauerstoff (O7+)", 
+            8, 
+            8 
+            // maximale Elektronenzahl in der ersten Schale ist 2, aber Sauerstoff hat insgesamt 8 Elektronen
         );
 
         double energy = 
-            hydrogen.calculateEnergyLevel(1);
+            hydrogen.calculateEnergyLevel(1, 1);
+            //Formel ist nur für wasserstoffähnliche Kerne gültig, daher verwenden wir n=1 für die erste Schale
 
         System.out.println(
             hydrogen.getName()
             + " : Energie in der ersten Schale : " + energy + " eV"
+            );
+
+        double heliumEnergy = 
+        // Berechnung der Energie für Helium in der ersten Schale
+        // Z = 2 für Helium, n = 1 für die erste Schale
+        // Helium hat 2 Protonen, daher Z = 2
+        // Energie gilt aber nur für wasserstoffähnliche Kerne, daher verwenden wir n=1 für die erste Schale
+            helium.calculateEnergyLevel(2, 1);
+
+        System.out.println(
+            helium.getName()
+            + " : Energie in der ersten Schale : " + heliumEnergy + " eV"
+            );
+
+        double oxygenEnergy = 
+        // Berechnung der Energie für Sauerstoff in der ersten Schale
+        // Z = 8 für Sauerstoff, n = 1 für die erste Schale
+            oxygen.calculateEnergyLevel(8, 1);
+
+        System.out.println(
+            oxygen.getName()
+            + " : Energie in der ersten Schale : " + oxygenEnergy + " eV"
             );
 
     }
