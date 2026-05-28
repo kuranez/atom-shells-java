fb0906d483460c457a93fb3a4454e7d4ebe3924d
Kuranez <kuranez420@proton.me>
2026-05-28 19:28:00 +0200

    Added Li and Be

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ public class Main {
         
         // System.out.println("Test!");
 
+        // Erstellen von Atom-Objekten
+
         Atom hydrogen = new Atom(
             "Wasserstoff (H)", 
             1, 
@@ -13,7 +15,18 @@ public class Main {
             "Helium (He+)", 
             2, 
             2 
-            // maximale Elektronenzahl in der ersten Schale ist 2, aber Helium hat insgesamt 2 Elektronen
+        );
+
+        Atom lithium = new Atom(
+            "Lithium (Li2+)", 
+            3, 
+            3 
+        );
+
+        Atom beryllium = new Atom(
+            "Beryllium (Be3+)", 
+            4, 
+            4 
         );
 
         Atom oxygen = new Atom(
@@ -23,9 +36,9 @@ public class Main {
             // maximale Elektronenzahl in der ersten Schale ist 2, aber Sauerstoff hat insgesamt 8 Elektronen
         );
 
+        //Berechnung der Energie für die erste Schale (n = 1) und Ausgabe der Ergebnisse
         double energy = 
             hydrogen.calculateEnergyLevel(1, 1);
-            //Formel ist nur für wasserstoffähnliche Kerne gültig, daher verwenden wir n=1 für die erste Schale
 
         System.out.println(
             hydrogen.getName()
@@ -34,26 +47,38 @@ public class Main {
 
         double heliumEnergy = 
         // Berechnung der Energie für Helium in der ersten Schale
-        // Z = 2 für Helium, n = 1 für die erste Schale
         // Helium hat 2 Protonen, daher Z = 2
-        // Energie gilt aber nur für wasserstoffähnliche Kerne, daher verwenden wir n=1 für die erste Schale
+        // Hauptquantenzahl n = 1 für die erste Schale
             helium.calculateEnergyLevel(2, 1);
 
         System.out.println(
             helium.getName()
             + " : Energie in der ersten Schale : " + heliumEnergy + " eV"
             );
+        
+        double lithiumEnergy = 
+            lithium.calculateEnergyLevel(3, 1);
+        
+        System.out.println(
+            lithium.getName()
+            + " : Energie in der ersten Schale : " + lithiumEnergy + " eV"
+            );
+        
+        double berylliumEnergy = 
+            beryllium.calculateEnergyLevel(4, 1);
+        
+        System.out.println(
+            beryllium.getName()
+            + " : Energie in der ersten Schale : " + berylliumEnergy + " eV"
+            );
 
         double oxygenEnergy = 
-        // Berechnung der Energie für Sauerstoff in der ersten Schale
-        // Z = 8 für Sauerstoff, n = 1 für die erste Schale
             oxygen.calculateEnergyLevel(8, 1);
 
         System.out.println(
             oxygen.getName()
             + " : Energie in der ersten Schale : " + oxygenEnergy + " eV"
             );
-
     }
 
 }
